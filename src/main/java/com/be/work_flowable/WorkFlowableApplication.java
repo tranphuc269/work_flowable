8ad93c0d21237b342db3795052ea5befae733215
Trần Văn Phúc <phuc260900@gmail.com>
2023-10-21 11:34:39 +0700

    Initial commit

---
+++ src/main/java/com/be/work_flowable/WorkFlowableApplication.java
@@ -0,0 +1,13 @@
+package com.be.work_flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFlowableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/be/work_flowable/WorkFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.work_flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
